595029f3357ef80c6722dc280b24a810465e0975
XMPPwocky <nttheis@gmail.com>
2014-08-16 05:53:19 +0000

    fixes

--- src/main.rs
+++ src/main.rs
@@ -18,26 +18,46 @@ fn main() {
   info!("nofunbot starting up...");
 
   NoFunBot::launch(Config { 
-    nick: "NoFunBot",
-    server: "irc.quakenet.org",
-    port: 6667
+    nick: "NoFunBot".to_string(),
+    server: "irc.quakenet.org".to_string(),
+    port: 6667,
+
+    channels: vec![Channel {
+      chantype: Moderate,
+      name: "#r/globaloffensive".to_string() 
+    }]
   });
 }
-
-pub struct Config<'a> {
-  nick: &'a str,
-  server: &'a str,
-  port: u16
+#[deriving(Clone)]
+pub enum ChannelType {
+  Moderate, // we mod this channel
+  Control // we are controlled here
+}
+#[deriving(Clone)]
+pub struct Channel {
+  name: String,
+  chantype: ChannelType
 }
 
-pub struct NoFunBot;
+#[deriving(Clone)]
+pub struct Config {
+  nick: String,
+  server: String,
+  port: u16,
+
+  channels: Vec<Channel>
+}
+
+pub struct NoFunBot {
+  config: Config
+}
 
 impl NoFunBot {
   pub fn launch(config: Config) {
-    let mut ircopts = irc::conn::Options::new(config.server, config.port);
-    ircopts.nick = config.nick.as_slice();
+    let mut bot = NoFunBot { config: config.clone() };
 
-    let mut bot = NoFunBot;
+    let mut ircopts = irc::conn::Options::new(config.server.as_slice(), config.port);
+    ircopts.nick = config.nick.as_slice();
 
     match irc::conn::connect(ircopts, (), |c,e,_| bot.handle(c, e)) {
       Ok(()) => info!("Exiting normally..."),
@@ -54,10 +74,11 @@ impl NoFunBot {
   pub fn handle_line(&mut self, conn: &mut Conn, line: Line) {
     match line {
       Line{command: IRCCode(1), ..} => {
-        println!("Logged in");
-        // we've logged in
-        conn.join(bytes!("#nofunbot"), [])
-      }
+        info!("Logged in");
+        for channel in self.config.channels.iter() {
+          conn.join(channel.name.as_bytes(), [])
+        }
+      },
       Line{command: IRCCmd(cmd), args, prefix: prefix } => match cmd.as_slice() {
         "JOIN" if prefix.is_some() => {
           let prefix = prefix.unwrap();
@@ -67,13 +88,12 @@ impl NoFunBot {
           if args.is_empty() {
             let line = Line{command: IRCCmd("JOIN".into_maybe_owned()),
             args: args, prefix: Some(prefix)};
-            println!("ERROR: Invalid JOIN message received: {}", line);
+            error!("ERROR: Invalid JOIN message received: {}", line);
             return;
           }
           let chan = args.move_iter().next().unwrap();
-          conn.privmsg(chan.as_slice(), bytes!("Hello"));
           let chan = String::from_utf8_lossy(chan.as_slice());
-          println!("JOINED: {}", chan);
+          info!("JOINED: {}", chan);
         }
         "PRIVMSG" | "NOTICE" => {
           let (src, dst, msg) = match prefix {
@@ -84,17 +104,16 @@ impl NoFunBot {
               (prefix.as_ref().unwrap().nick(), dst, msg)
             }
             _ => {
-              print!("ERROR: Unexpected {} line: ", cmd);
+              error!("ERROR: Unexpected {} line: ", cmd);
               let line = Line{command: IRCCmd(cmd), args: args, prefix: prefix};
-              println!("{}", line);
+              error!("{}", line);
               return;
             }
           };
-          let dsts = String::from_utf8_lossy(dst.as_slice());
-          let srcs = String::from_utf8_lossy(src.as_slice());
-          let msgs = String::from_utf8_lossy(msg.as_slice());
-          println!("<-- {}({}) {}: {}", cmd, dsts, srcs, msgs);
-          self.handle_privmsg(conn, msg.as_slice(), src, dst.as_slice())
+          let dsts = String::from_utf8_lossy(dst.as_slice()).into_string();
+          let srcs = String::from_utf8_lossy(src.as_slice()).into_string();
+          let msgs = String::from_utf8_lossy(msg.as_slice()).into_string();
+          self.handle_privmsg(conn, msgs, srcs, dsts)
         }
         _ => ()
       },
@@ -106,19 +125,35 @@ impl NoFunBot {
           }
           _ => {
             let line = Line{command: IRCAction(dst), args: args, prefix: prefix};
-            println!("ERROR: Unexpected ACTION line: {}", line);
+            error!("ERROR: Unexpected ACTION line: {}", line);
             return;
           }
         };
         let dst = String::from_utf8_lossy(dst.as_slice());
         let src = String::from_utf8_lossy(src.as_slice());
         let msg = String::from_utf8_lossy(msg.as_slice());
-        println!("<-- PRIVMSG({}) {} {}", dst, src, msg);
       }
       _ => ()
     }
   }
-  pub fn handle_privmsg(&mut self, conn, msg: &[u8], src: &[u8], dst: &[u8]) {
-      info!("message");
+  pub fn handle_privmsg(&mut self, conn: &mut Conn, msg: String, src: String, dst: String) {
+    info!("{} -> {}: {}", src, dst, msg);
+
+    if dst.as_slice().starts_with("#") {
+      match self.config.channels.iter().find(|chan| chan.name == dst) {
+        Some(channel) => {
+          self.moderate(conn, src, channel.clone(), msg)
+        },
+        None => {
+          debug!("Silently ignoring...");
+        }
+      }
+    }
+  }
+
+  pub fn moderate(&self, conn: &mut Conn, nick: String, channel: &Channel, msg: String) {
+      if msg.as_slice() == "hi i'm beasway" {
+         conn.privmsg(channel.name.as_bytes(), b"BOOO!");
+      }
   }
 }
